4e985ec5117aa1008894b4cfcf7f5f02c2cc4887
CaioAdriano1199 <caioadriano1199@gmail.com>
2026-05-15 22:46:24 -0300

    login mudado

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -7,6 +7,7 @@ export default function Login() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
   const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
   const infologin = {
@@ -19,7 +20,8 @@ export default function Login() {
       const resposta = await fetch(`${BASE_URL}/usuarios/login`, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          "bypass-tunnel-reminder": "true"
         },
         body: JSON.stringify(infologin)
 
@@ -46,38 +48,38 @@ export default function Login() {
 
     <main className="bg-[url('/fundoAlt.png')] bg-cover bg-center bg-no-repeat min-h-screen">
       <div className="flex min-h-screen flex-col items-center justify-center bg-[var(--azultransp)] backdrop-blur-sm w-full h-full absolute top-0 left-0">
-      <div className="gradient-vertical left-0"></div>
-      <div className="gradient-vertical right-0"></div>
-      <div className="relative z-10 max-w-5xl mx-auto p-4">
-      <div className="flex w-100 flex-col justify-center p-10 mt-6 bg-[var(--branco)] rounded-lg shadow-md">
-        <h1 className="self-center p-5 text-3xl font-semibold text-black">
-          Relic
-        </h1>
+        <div className="gradient-vertical left-0"></div>
+        <div className="gradient-vertical right-0"></div>
+        <div className="relative z-10 max-w-5xl mx-auto p-4">
+          <div className="flex w-100 flex-col justify-center p-10 mt-6 bg-[var(--branco)] rounded-lg shadow-md">
+            <h1 className="self-center p-5 text-3xl font-semibold text-black">
+              Relic
+            </h1>
 
-        <input
-          type="email"
-          placeholder="Email"
-          className="border bg-[var(--cinzaclaro)] border-gray-300 rounded-md my-2.5 py-2 px-4 text-[var(--preto)] focus:outline-none focus:ring-2 focus:ring-[var(--bgbutton)] placeholder:text-[var(--phgray)]"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="border bg-[var(--cinzaclaro)] border-gray-300 rounded-md my-2.5 py-2 px-4 text-[var(--preto)]  focus:outline-none focus:ring-2 focus:ring-[var(--bgbutton)] placeholder:text-[var(--phgray)]"
-          onChange={(e) => setSenha(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="bg-[var(--bgbutton)] hover:bg-[var(--bgbuttonhover)] my-2.5 text-[var(--branco)] font-bold py-2 px-4 rounded-md hover:cursor-pointer transition-colors duration-300"
-          onClick={login}
-        >
-          Login
-        </button>
-        <p className="self-center p-5 text-sm text-[var(--cinzaescuro)] font-semibold">
-          Não tem uma conta? <a href="/cadastro" className="text-[var(--bgbutton)] hover:underline">Cadastre-se</a>
-        </p>
-      </div>
-      </div>
+            <input
+              type="email"
+              placeholder="Email"
+              className="border bg-[var(--cinzaclaro)] border-gray-300 rounded-md my-2.5 py-2 px-4 text-[var(--preto)] focus:outline-none focus:ring-2 focus:ring-[var(--bgbutton)] placeholder:text-[var(--phgray)]"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="border bg-[var(--cinzaclaro)] border-gray-300 rounded-md my-2.5 py-2 px-4 text-[var(--preto)]  focus:outline-none focus:ring-2 focus:ring-[var(--bgbutton)] placeholder:text-[var(--phgray)]"
+              onChange={(e) => setSenha(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="bg-[var(--bgbutton)] hover:bg-[var(--bgbuttonhover)] my-2.5 text-[var(--branco)] font-bold py-2 px-4 rounded-md hover:cursor-pointer transition-colors duration-300"
+              onClick={login}
+            >
+              Login
+            </button>
+            <p className="self-center p-5 text-sm text-[var(--cinzaescuro)] font-semibold">
+              Não tem uma conta? <a href="/cadastro" className="text-[var(--bgbutton)] hover:underline">Cadastre-se</a>
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   )
